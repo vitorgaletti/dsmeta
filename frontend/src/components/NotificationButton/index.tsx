5c7958d5ad414a1f3e0269d9dedc7f9dd09508a5
Vitor <vitor.fgaletti@gmail.com>
2022-09-28 21:33:00 -0300

    ✨ Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,28 @@
 import './styles.css';
 
-import icon from '../../assets/img/notification-icon.svg';
+import axios from 'axios';
 
-export function NotificationButton() {
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+
+interface NotificationButtonProps {
+  saleId: number;
+}
+
+export function NotificationButton({ saleId }: NotificationButtonProps) {
+  function handleClick(id: number) {
+    axios.get(`${BASE_URL}/sales/${id}/notification`).then(response => {
+      console.log('Sucesso');
+    });
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div
+      className="dsmeta-red-btn"
+      onClick={() => {
+        handleClick(saleId);
+      }}
+    >
       <img src={icon} alt="Notificar" />
     </div>
   );
